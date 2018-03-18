141003c35e822b0f9a2f11ac3681e5321ee215ce
Fahim Mulla <fahimmulla@Fahims-MacBook-Pro.local>
2018-03-18 20:14:10 +0000

    Keep practising

---
+++ src/Dowhile.java
@@ -0,0 +1,17 @@
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i = 10;
+		do { 
+			System.out.println("Hello");
+		}
+		while (i<10);
+	}
+
+}
+
+//while loop will always excute once.
---
+++ src/javapractice/ForintSum.java
@@ -0,0 +1,19 @@
+package javapractice;
+
+public class ForintSum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int sum = 0;
+				
+		for (int i = 0; i <= 10; i++) {
+					
+					sum = sum +i;
+				}
+		
+		System.out.println("The sum is : " + sum);
+	}
+
+}
